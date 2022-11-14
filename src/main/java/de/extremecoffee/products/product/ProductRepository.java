f37e9e16775a27a21aa7689f5d137c51624dd9dc
Vincent Dlugosch <dlugoschvincent@gmail.com>
2022-11-14 02:58:47 +0100

    chore: change inherited interface of ProductRepository to include more features

--- src/main/java/de/extremecoffee/products/product/ProductRepository.java
+++ src/main/java/de/extremecoffee/products/product/ProductRepository.java
@@ -1,8 +1,10 @@
 package de.extremecoffee.products.product;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface ProductRepository extends CrudRepository <Product, Long> {
+@Repository
+public interface ProductRepository extends JpaRepository <Product, Long> {
   
   Product findById(long id);
 }
