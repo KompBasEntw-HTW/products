7c06e279e58eaa6f847931af018c7b4b1aa32f03
Vincent Dlugosch <dlugoschvincent@gmail.com>
2022-11-14 02:55:25 +0100

    feat: add required endpoints and validation to products controller

--- src/main/java/de/extremecoffee/products/controller/ProductsController.java
+++ src/main/java/de/extremecoffee/products/controller/ProductsController.java
@@ -1,28 +1,36 @@
 package de.extremecoffee.products.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import de.extremecoffee.products.product.Product;
 import de.extremecoffee.products.product.ProductRepository;
-
-import org.springframework.web.bind.annotation.RequestMethod;
+import jakarta.validation.Valid;
 
 @RestController
 public class ProductsController {
   @Autowired
   ProductRepository productRepository;
 
-  @RequestMapping(value = "products", method = RequestMethod.GET)
-  public ResponseEntity<List<Product>> products() {
-    var products = new ArrayList<Product>();
-    productRepository.findAll().forEach(products::add);
-    return new ResponseEntity<>(products, HttpStatus.OK);
+  @GetMapping("/")
+  List<Product> all() {
+    return productRepository.findAll();
+  }
+
+  @PostMapping("/")
+  Product newProduct(@Valid @RequestBody Product newProduct) {
+    return productRepository.save(newProduct);
+  }
+
+  @DeleteMapping("/{id}")
+  void deleteProduct(@PathVariable Long id) {
+    productRepository.deleteById(id);
   }
 }
