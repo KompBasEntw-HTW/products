4108f84bfe8df0c95db568f5a9cf6ce758117541
Vincent Dlugosch <dlugoschvincent@gmail.com>
2022-11-14 02:59:21 +0100

    chore: set openapi base url so swagger ui produces correct requests

--- src/main/java/de/extremecoffee/products/ProductsApplication.java
+++ src/main/java/de/extremecoffee/products/ProductsApplication.java
@@ -3,6 +3,15 @@ package de.extremecoffee.products;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition( 
+    servers = {
+       @Server(url = "/api/products", description = "Default Server URL")
+    }
+) 
+
 @SpringBootApplication
 public class ProductsApplication {
 
